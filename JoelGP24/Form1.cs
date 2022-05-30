78f929da263cedf2a7cb329bdcaf02ac32a46083
daw1sm <daw1sm@A406>
2022-05-30 20:45:13 +0200

    labels

--- JoelGP24/Form1.cs
+++ JoelGP24/Form1.cs
@@ -17,24 +17,5 @@ namespace JoelGP24
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label3_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
     }
 }
